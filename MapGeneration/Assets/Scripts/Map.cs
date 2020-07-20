7dca760805801d9c778ec2104fc582147826128f
Dexter <itsdexter66@gmail.com>
2020-07-20 21:20:30 +0200

    Changed some meta files

--- MapGeneration/Assets/Scripts/Map.cs
+++ MapGeneration/Assets/Scripts/Map.cs
@@ -1,12 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+
+public enum TileType
+{
+    Empty = -1,
+    Grass = 15,
+    Tree  = 16,
+    Hills = 17,
+    Mountains = 18
+}
 
 public class Map
 {
     public Tile[] tiles;
     public int columns;
     public int rows;
+
+    public Tile[] coastTiles
+    {
+        get
+        {
+            return tiles.Where(t => t.autotileID < (int)TileType.Grass).ToArray();
+        }
+    }
+
+    public Tile[] landTiles
+    {
+        get
+        {
+            return tiles.Where(t => t.autotileID == (int)TileType.Grass).ToArray();
+        }
+    }
 
     public void NewMap(int width, int height)
     {
@@ -18,6 +44,18 @@ public class Map
         CreateTiles();
     }
 
+    public void CreateIsland(float erodePercent, int erodeIterations, float treePercent, float hillPercent, float mountainPercent)
+    {
+        for(var i = 0; i < erodeIterations; i++)
+        {
+            DecorateTiles(coastTiles, erodePercent, TileType.Empty);
+        }
+
+        DecorateTiles(landTiles, treePercent, TileType.Tree);
+        DecorateTiles(landTiles, hillPercent, TileType.Hills);
+        DecorateTiles(landTiles, mountainPercent, TileType.Mountains);
+    }
+
     private void CreateTiles()
     {
         var total = tiles.Length;
@@ -27,6 +65,70 @@ public class Map
             var tile = new Tile();
             tile.id = i;
             tiles[i] = tile;
+        }
+
+        FindNeighbors();
+    }
+
+    private void FindNeighbors()
+    {
+        for(var r = 0; r < rows; r++)
+        {
+            for(var c = 0; c < columns; c++)
+            {
+                var tile = tiles[columns * r + c];
+
+                if (r < rows - 1)
+                {
+                    tile.AddNeighbor(Sides.Bottom, tiles[columns * (r + 1) + c]);
+                }
+
+                if (c < columns - 1)
+                {
+                    tile.AddNeighbor(Sides.Right, tiles[columns * r + c + 1]);
+                }
+
+                if (c > 0)
+                {
+                    tile.AddNeighbor(Sides.Left, tiles[columns * r + c - 1]);
+                }
+
+                if (r > 0)
+                {
+                    tile.AddNeighbor(Sides.Top, tiles[columns * (r - 1) + c]);
+                }
+            }
+        }
+    }
+
+    public void DecorateTiles(Tile[] tiles, float percent, TileType type)
+    {
+        var total = Mathf.FloorToInt(tiles.Length * percent);
+
+        RandomizeTileArray(tiles);
+        
+        for(var i = 0; i < total; i++)
+        {
+            
+            var tile = tiles[i];
+
+            if (type == TileType.Empty)
+            {
+                tile.ClearNeighbors();
+
+                tile.autotileID = (int)type;
+            }
+        }
+    }
+
+    public void RandomizeTileArray(Tile[] tiles)
+    {
+        for (var i = 0; i < tiles.Length; i++)
+        {
+            var tmp = tiles[i];
+            var r = Random.Range(i, tiles.Length);
+            tiles[i] = tiles[r];
+            tiles[r] = tmp;
         }
     }
 }
--- MapGeneration/Assets/Scripts/RandomMapTester.cs
+++ MapGeneration/Assets/Scripts/RandomMapTester.cs
@@ -18,6 +18,18 @@ public class RandomMapTester : MonoBehaviour
     [Header("Map sprites")]
     public Texture2D islandTexture;
 
+    [Space]
+    [Header("Decorate map")]
+    [Range(0, .9f)]
+    public float erodePercent = .5f;
+    public int erodeIterations = 2;
+    [Range(0, .9f)]
+    public float treePercent = .3f;
+    [Range(0, .9f)]
+    public float hillPercent = .2f;
+    [Range(0, .9f)]
+    public float mountainsPercent = .1f;
+
     public Map map;
 
     // Start is called before the first frame update
@@ -29,7 +41,7 @@ public class RandomMapTester : MonoBehaviour
     public void MakeMap()
     {
         map.NewMap(mapWidth, mapHeight);
-        Debug.Log("created a new map" + map.columns + "*" + map.rows);
+        map.CreateIsland(erodePercent, erodeIterations, treePercent, hillPercent, mountainsPercent);
         CreateGrid();
     }
 
@@ -55,10 +67,15 @@ public class RandomMapTester : MonoBehaviour
             go.transform.SetParent(mapContainer.transform);
             go.transform.position = new Vector3(newX, newY, 0);
 
-            var spriteID = 0;
-            var sr = go.GetComponent<SpriteRenderer>();
-            sr.sprite = sprites[spriteID];
+            var tile = map.tiles[i];
+            var spriteID = tile.autotileID;
 
+            if (spriteID >= 0)
+            {
+                var sr = go.GetComponent<SpriteRenderer>();
+                sr.sprite = sprites[spriteID];
+            }
+            
             if (column == (maxColumns - 1))
             {
                 row++;
